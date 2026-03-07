9455c6cf6d6a104f40745dce1cf56d5bdf23fb43
Himal <purihimal9@gmail.com>
2026-03-07 17:40:08 +0100

    initializing

---
+++ src/main/java/com/jagir/jagir/JagirApplication.java
@@ -0,0 +1,13 @@
+package com.jagir.jagir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JagirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jagir/jagir/JagirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jagir.jagir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JagirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
